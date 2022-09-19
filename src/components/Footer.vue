2203e64f411bd4735d140aa78ac1b7d619897320
dudematthew <mmoczydlowski4@gmail.com>
2022-09-19 12:34:14 +0200

    Update footer - fix unproper MIT explanation

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,9 @@
         <div class="content has-text-centered">
             <p>
                 <strong>BOOST</strong> stworzony przez <a href="https://github.com/dudematthew" target="_blank">Mateusza
-                    Moczydłowskiego</a>. System ten jest
-                <strong>absolutnie nie licencjonowany</strong> i możesz go modyfikować i używać w swoich kreacjach jak
-                tylko dusza zapragnie. Strona natomiast na licencji <a
-                    href="http://opensource.org/licenses/mit-license.php" target="_blank">MIT</a>.
+                    Moczydłowskiego</a>. Zarówno system jak i strona są
+                na licencji <a href="http://opensource.org/licenses/mit-license.php" target="_blank">MIT</a>. Możesz
+                je modyfikować i używać w swoich kreacjach jak tylko dusza zapragnie ale podlinkuj gdzieś tę stronę.
             </p>
         </div>
     </footer>
