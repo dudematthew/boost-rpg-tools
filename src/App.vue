b08fb96c35a3ae4a195b6c7f0a515ecc5e87a460
dudematthew <mmoczydlowski4@gmail.com>
2022-09-24 23:26:08 +0200

    Add shake class and D6Button component

--- src/App.vue
+++ src/App.vue
@@ -387,6 +387,28 @@
     flex: 1 1 0 !important;
   }
 
+  .shake {
+    animation: shake 0.82s cubic-bezier(.36, .07, .19, .97) both;
+  }
+
+  @keyframes shake {
+    10%, 90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+    
+    20%, 80% {
+      transform: translate3d(2px, 0, 0);
+    }
+
+    30%, 50%, 70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+
+    40%, 60% {
+      transform: translate3d(4px, 0, 0);
+    }
+  }
+
   // Customize vue-select css
   :root {
     --vs-colors--lightest: rgba(60, 60, 60, 1);
---
+++ src/components/D6Button.vue
@@ -0,0 +1,69 @@
+<script>
+    import Randomizer from '@/lib/randomizer'
+    let randomizer = new Randomizer()
+
+    export default {
+        name: 'D6Button',
+        props: [
+            'other',
+            'baseHP'
+        ],
+        data() {
+            return {
+                currentIcon: "fa-dice-d6",
+                shake: false
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            async roll() {
+                let result = await randomizer.getRandomNumber(1, 6);
+
+                this.shake = true;
+
+                setTimeout(() => {
+                    this.shake = false;
+
+                    switch (parseInt(result)) {
+                        case 1:
+                            this.currentIcon = "fa-dice-one";
+                            break;
+                        case 2:
+                            this.currentIcon = "fa-dice-two";
+                            break;
+                        case 3:
+                            this.currentIcon = "fa-dice-three";
+                            break;
+                        case 4:
+                            this.currentIcon = "fa-dice-four";
+                            break;
+                        case 5:
+                            this.currentIcon = "fa-dice-five";
+                            break;
+                        case 6:
+                            this.currentIcon = "fa-dice-six";
+                            break;
+                            
+                        default:    
+                            this.currentIcon = "fa-wifi";
+                            console.error("Throw failed: ", result);
+                            break;
+                }
+                }, 500);
+
+            },
+        },
+        watch: {
+        },
+    }
+</script>
+
+<template>
+    <button class="button" @click="roll()">
+      <span class="icon is-small" :class="{'shake': shake}">
+        <i class="fa-solid" :class="currentIcon"></i>
+      </span>
+    </button>
+</template>
