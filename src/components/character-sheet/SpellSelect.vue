cb8bca338bd942bcc68773f3fc168fb8015bba52
dudematthew <mmoczydlowski4@gmail.com>
2022-11-19 15:49:58 +0100

    Fix spell multiselect in CharacterSheetView, make it almost perfect

--- src/components/character-sheet/SpellSelect.vue
+++ src/components/character-sheet/SpellSelect.vue
@@ -1,106 +1,143 @@
 <script>
-import { nextTick } from '@vue/runtime-core';
-export default {
-    name: "SpellSelect",
-    props: [
-        'spellList',
-        'availableSpells',
-        'spellAmount'
-    ],
+    import {
+        nextTick
+    } from '@vue/runtime-core';
+    export default {
+        name: "SpellSelect",
+        props: [
+            'spellList',
+            'availableSpells',
+            'spellAmount'
+        ],
 
-    data () {
-        return {
-            expanded: false,
-        }
-    },
-
-    methods: {
-        onSelectChange (e) {
-            let selectEl = e.target;
-
-            for (let i = 0; i < selectEl.length; i++) {
-                let option = selectEl.options[i];
-
-                this.spellList[option.value].chosen = option.selected;
-
-            }
-
-            console.log(this.spellList)
-        },
-
-        spellAvailable (spellId) {
-            console.log(this.spellListLength);
-            return this.availableSpells.includes(spellId);
-        },
-
-        updateSelectOptions () {
-            let selectEl = this.$refs.select;
-
-            for (let i = 0; i < selectEl.length; i++) {
-                let option = selectEl.options[i];
-
-                if (this.spellList[option.value].chosen)
-                    option.removeAttribute("selected");
-                else
-                    option.selected = "selected";
-
+        data() {
+            return {
+                expanded: false,
+                lastAddedSpell: 'null',
             }
         },
 
-        // clearDataChoices () {
-        //     for (let dId in this.data) {
-        //         this.data[dId][this.switchAttrib] = false;
-        //     }
-        // },
-    },
+        methods: {
+            /**
+             * Update data based on selected options
+             * todo: check if character can have given spell
+             * @param {*} e event
+             */
+            onSelectChange(e) {
+                let selectEl = e.target;
 
-    computed: {
-        spellListLength () {
-            return this.availableSpells.length;
-        }
-    },
+                for (let i = 0; i < selectEl.length; i++) {
+                    let option = selectEl.options[i];
 
-    watch: {
-        /**
-         * todo: currently something's wrong with oldSpellList which isn't actually old
-         */
-        spellList: {
-            handler(newSpellList, oldSpellList) {
-                console.log("Oldspellist: ", oldSpellList);
+                    if (this.spellAvailable(option.value)) {
+                        this.spellList[option.value].chosen = option.selected;
+                        option.selected = this.spellList[option.value].chosen;
+                    } else {
+                        option.selected = false;
+                        this.spellList[option.value].chosen = false;
+                    }
+
+                }
+
+                this.updateSelectOptions();
+
+                console.log(this.spellList);
+            },
+
+            // Get option HTML element by spell id
+            getOptionBySpellId(spellId) {
+                let selectEl = this.$refs.select;
+
+                // for (let i = 0; i < selectEl.length; i++) {
+
+                // }
+
+                for (let x of Array.from(selectEl.children)[0]) {
+                    let option = x;
+
+
+                    if (option.value == spellId) {
+                        return option;
+                    }
+
+                }
+
+                console.error("Couldn't find spell: ", spellId);
+                return null;
+            },
+
+            // Check if current class can have given spell
+            spellAvailable(spellId) {
+                console.log(this.spellListLength);
+                return this.availableSpells.includes(spellId);
+            },
+
+            /**
+             * Check if spell amount is greater than
+             * maximum spell amount
+             */
+            hasTooMuch() {
                 let chosenSpellsAmount = 0;
 
-                for (let spellId in newSpellList) {
-                    let spell = newSpellList[spellId];
+                for (let spellId in this.spellList) {
+                    let spell = this.spellList[spellId];
 
                     if (spell.chosen)
                         chosenSpellsAmount++
                 }
 
-                if (chosenSpellsAmount > this.spellAmount) {
-                    console.log("Replacing...", oldSpellList);
-                    this.updateSelectOptions();
-                    return oldSpellList;
-                }
-                else
-                    return newSpellList;
+                return chosenSpellsAmount > this.spellAmount;
             },
-            deep: true
-        }
-    },
 
-    mounted () {
-        console.log("SpellSelect: ", this.spellList, this.availableSpells);
+            /**
+             * Update selection based on data
+             * Unselect spells if character can't
+             * have more
+             */
+            updateSelectOptions() {
+                if (this.hasTooMuch()) {
+                    if (!!this.lastAddedSpell) {
+                        console.log("LOG: ", this.getOptionBySpellId(this.lastAddedSpell), this.lastAddedSpell);
+                        this.getOptionBySpellId(this.lastAddedSpell).selected = false;
+                    }
+                }
+            },
+
+            /**
+             * Update last added spell
+             * @param {*} spellId spell that is clicked
+             */
+            updateLastAddedSpell(spellId, e) {
+                if (e.target.selected) {
+                    this.lastAddedSpell = spellId;
+                    this.spellList[spellId].chosen = true;
+                }
+
+                console.log(spellId, this.lastAddedSpell, this.spellList[spellId].chosen);
+            },
+        },
+
+        computed: {
+            spellListLength() {
+                return this.availableSpells.length;
+            }
+        },
+
+        mounted() {
+            console.log("SpellSelect: ", this.spellList, this.availableSpells);
+        }
     }
-}
 </script>
 
 <template>
     <div class="field is-grouped" tabindex="1" @focusout="expanded = false" v-if="availableSpells.length != 0">
         <div class="control select-control" @click="expanded = true">
             <div class="select is-multiple is-fullwidth is-large" ref="select">
-                <select @change="onSelectChange($event)" :size="(expanded) ? spellListLength : 1" style="overflow: hidden" multiple>
+                <select @change="onSelectChange($event)" :size="expanded ? spellListLength : 1" style="overflow: hidden"
+                    multiple>
                     <template v-for="(spell, spellId) in spellList" :key="spellId">
-                        <option :selected="spell.chosen" :value="spellId" v-if="spellAvailable(spellId)">
+                        <option :selected="spell.chosen" :value="spellId" v-if="spellAvailable(spellId)"
+                            @click="updateLastAddedSpell(spellId, $event)">
                             {{spell.name}}
                         </option>
                     </template>
@@ -110,7 +147,7 @@ export default {
         <div class="control">
             <button class="button" @click="expanded = !expanded">
                 <span class="icon is-small">
-                <i class="fa-solid" :class="{'fa-chevron-down': !expanded, 'fa-chevron-up': expanded}"></i>
+                    <i class="fa-solid" :class="{'fa-chevron-down': !expanded, 'fa-chevron-up': expanded}"></i>
                 </span>
             </button>
         </div>
@@ -130,10 +167,12 @@ export default {
         z-index: 12;
     }
 
+    .select option:checked {
+        background-color: #0078D7
+    }
+
     .select-control {
         display: block;
         width: calc(100% - 40px);
     }
 </style>
-
-
